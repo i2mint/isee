f159251c7645a07d26fe223246da1cf62548f9eb
Valentin Féron <vferon@pentalog.com>
2021-07-15 14:13:11 -0500

    Removed ssh url tranform

--- isee/git_utils.py
+++ isee/git_utils.py
@@ -5,7 +5,6 @@ from isee.common import git
 
 
 def check_in(commit_message='No commit message.'):
-    _prepare_to_push()
     current_changes = git('status', '--porcelain=v1')
     if current_changes:
         branch = os.environ['CI_COMMIT_REF_NAME']
@@ -17,18 +16,7 @@ def check_in(commit_message='No commit message.'):
 
 
 def tag_repo(tag: str, git_dir: str = None):
-    _prepare_to_push(git_dir)
-    git('tag', tag)
-    git('push', 'origin', tag)
-
-
-def _prepare_to_push(git_dir=None):
     if git_dir:
         os.chdir(git_dir)
-    # Transforms the repository URL to the SSH URL if needed
-    url = git('remote', 'get-url', '--push', 'origin')
-    if 'github.com' not in url:
-        http_url_regex = r'https?:\/\/(.*:.*@)?([^\/]+)\/'
-        ssh_url = re.sub(http_url_regex, r'git@\2:', url)
-        if url != ssh_url:
-            git('remote', 'set-url', '--push', 'origin', ssh_url)
+    git('tag', tag)
+    git('push', 'origin', tag)
