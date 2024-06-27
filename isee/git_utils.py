66bd7b0c572997cd0aa4ea7a8f99dad4a38bc373
Thor Whalen <thorwhalen1@gmail.com>
2024-06-27 11:32:52 +0200

    fix: tag_repo

--- isee/git_utils.py
+++ isee/git_utils.py
@@ -5,12 +5,13 @@ Functions:
 - tag_repo: Tag the git repository with a new tag and push it to the remote repository.
 
 """
+
 import os
 
 from isee.common import git
 
 
-def tag_repo(tag: str, *, git_dir: str = None):
+def tag_repo(tag: str, *, git_dir: str = '.'):
     # if git_dir:
     #     os.chdir(git_dir)
     git('tag', tag, work_tree=git_dir)
